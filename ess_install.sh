2be7347baef73d3da4d25311d084214b40985693
uriel <zhuoshengjiang@icloud.com>
2016-05-14 20:07:54 -0700

    SECOND VERSION OF ESSENTIAL INSTALL

--- ess_install.sh
+++ ess_install.sh
@@ -1,3 +1,5 @@
+sudo apt-get update
 sudo apt-get install -y build-essential
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
+sudo npm install babel-preset-es2015 -g
