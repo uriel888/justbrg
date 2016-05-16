4eb5b41e419423eab498841b9da6d5d81a138ba4
uriel <zhuoshengjiang@icloud.com>
2016-05-15 19:09:09 -0700

    aaa

--- ess_install.sh
+++ ess_install.sh
@@ -4,3 +4,4 @@ curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo npm install babel-preset-es2015 -g
 sudo npm install -g babel-cli
+npm install babel-preset-es2015 --no-bin-links --save
