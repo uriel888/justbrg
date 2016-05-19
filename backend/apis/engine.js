7b68d549f8aa0dbeb291865cc96e10f1ba0bbb12
uriel888 <zhuoshengjiang@icloud.com>
2016-05-19 15:52:06 -0400

    aa

--- backend/apis/engine.js
+++ backend/apis/engine.js
@@ -1,10 +1,15 @@
-import  express       from  "express"
-import  passport      from  "passport"
-import  * as  master  from  "../configs/master.json"
-import  User          from  "../models/users.js"
+import express from "express"
+import passport from "passport"
+import * as master from "../configs/master.json"
+import User from "../models/users.js"
+import {
+  list as convert
+} from "./tools/countryConverter.js"
+
+
 let router = express.Router()
 
-if(master.Status == "dev"){
+if (master.Status == "dev") {
   router.get(`/test${master.Status}`, (req, res) => {
     res.end("GOOD TEST");
   });
@@ -13,5 +18,32 @@ if(master.Status == "dev"){
     res.send(req.user);
   });
 }
+
+
+router.post('/', (req, res) => {
+  //Date format: mm/dd/yyyy
+  if (!req.query.checkin) {
+    return res.status(500).send("checkin Wrong");
+  } else if (!req.query.checkout) {
+    return res.status(500).send("checkout Wrong");
+  } else if (!req.query.city) {
+    return res.status(500).send("city Wrong");
+  } else if (!req.query.source) {
+    return res.status(500).send("source Wrong");
+  }
+
+  country = '';
+  if (req.query.country) {
+    req.query.country = req.query.country.toLowerCase();
+    req.query.country = convert[req.query.country];
+    if (req.query.country == undefined) {
+      return res.status(500).send("Country wrong")
+    }
+    country = req.query.country;
+  }
+
+
+
+});
 
 module.exports = router;
