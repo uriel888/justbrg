8df4278ccd8bf940be0288570e9f8db66f526ecc
uriel <zhuoshengjiang@icloud.com>
2016-05-19 15:04:14 -0700

    PhantomJS install

--- ess_install.sh
+++ ess_install.sh
@@ -8,7 +8,7 @@ sudo npm install babel-preset-es2015 -g
 sudo npm install -g babel-cli
 npm install babel-preset-es2015 --no-bin-links --save
 cd ~
-export PHANTOM_JS="phantomjs-1.9.8-linux-x86_64"
+export PHANTOM_JS="phantomjs-2.1.1-linux-x86_64"
 wget https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOM_JS.tar.bz2
 sudo tar xvjf $PHANTOM_JS.tar.bz2
 sudo mv $PHANTOM_JS /usr/local/share
