2b254bd2c524077d65ac902dab9f5d442a036d45
uriel888 <zhuoshengjiang@icloud.com>
2016-05-19 17:26:45 -0400

    c

--- backend/crawler.js
+++ backend/crawler.js
@@ -3,12 +3,14 @@ import passport from "passport"
 import morgan from "morgan"
 import bodyParser from "body-parser"
 import * as master from "./configs/master.json"
+import Horseman from "node-horseman"
 import {
   encrypt,
   decrypt
 } from "./tools/crypt.js"
 
 
+let horseman = new Horseman();
 let app = express();
 let port = process.env.PORT || master.dev_port;
 
@@ -26,7 +28,15 @@ app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
 }));
 
 app.get('/:encode', (req, res) => {
-  res.end(decrypt(req.params.encode));
+  let url = decrypt(req.params.encode);
+  //TODO: ADD VERIFICATION
+
+  horseman
+  .open(url)
+  .then((text)=>{
+    res.end(text);
+  })
+  .close();
 });
 app.listen(port);
 console.log(`Server ${master.Status} is listening on ${port}`);
