cadc1f8c75c5d743ac7917e03186338de44b33d8
uriel888 <zhuoshengjiang@icloud.com>
2016-05-19 21:06:39 -0400

    d

--- backend/crawler.js
+++ backend/crawler.js
@@ -40,11 +40,11 @@ app.get('/:encode', (req, res) => {
     .open(url)
     .text('.propertyInner')
     .then((text) => {
-      text = text.split('\n');
-      for (let a = 0; a < text.length; a++) {
-        console.log(a + " : " + text[a].trim());
-      }
-      res.end(text);
+      // text = text.split('\n');
+      // for (let a = 0; a < text.length; a++) {
+      //   console.log(a + " : " + text[a].trim());
+      // }
+      res.end(text.trim());
     })
     .close();
 });
