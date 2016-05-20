0d0f7e94314edc74b2e73e2523f8a65244c4eae5
uriel888 <zhuoshengjiang@icloud.com>
2016-05-19 21:10:50 -0400

    d

--- backend/crawler.js
+++ backend/crawler.js
@@ -40,7 +40,7 @@ app.get('/:encode', (req, res) => {
     .open(url)
     .text('.propertyInner')
     .then((text) => {
-      text = text.split("\n+");
+      text = text.split(/\r?\n/);
       for (let a = 0; a < text.length; a++) {
         console.log(a + " : " + text[a].trim());
       }
