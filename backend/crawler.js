d1c6f275ae6a0ed117b4dcc3593ffb7e9d76664c
uriel888 <zhuoshengjiang@icloud.com>
2016-05-20 02:16:20 -0400

    d

--- backend/crawler.js
+++ backend/crawler.js
@@ -43,7 +43,7 @@ app.get('/:encode', (req, res) => {
       .open(url)
       .text('.propertyInner')
       .then((text) => {
-        text = text.replace(/Dates flexible\\?/g,'').replace(/Find Availability/g,'').replace(/\n/g, ' ').trim().replace(/\s\s+/g, '\n')
+        text = text.replace(/Dates flexible\?/g,'').replace(/Find Availability/g,'').replace(/\n/g, ' ').trim().replace(/\s\s+/g, '\n')
         let count = 0;
         let start = 0;
         let end = 0;
