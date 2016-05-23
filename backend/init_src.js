a25ec4c816b97ede263be03e6d55184a2206990f
uriel888 <zhuoshengjiang@icloud.com>
2016-05-23 14:34:27 -0400

    d

--- backend/init_src.js
+++ backend/init_src.js
@@ -58,8 +58,10 @@ console.log("Current Mode:" + master.Status);
 if (master.Status === "dev") {
   app.use(morgan(`${master.Status}`));
   app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
     next();
   });
 } else {
