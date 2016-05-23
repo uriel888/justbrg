9923be3e8f72ae1e092efeb2251bb9546b65941d
uriel888 <zhuoshengjiang@icloud.com>
2016-05-23 17:19:07 -0400

    d

--- backend/init_src.js
+++ backend/init_src.js
@@ -82,7 +82,7 @@ if (master.Status === "dev") {
   //   }));
   app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', "localhost:8080");
+    res.header('Access-Control-Allow-Origin', "http://localhost:8080");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
     next();
