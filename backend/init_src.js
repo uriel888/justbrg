de1a545b9021b2bb822399629dc639f90af114e4
uriel888 <zhuoshengjiang@icloud.com>
2016-05-23 17:18:31 -0400

    d

--- backend/init_src.js
+++ backend/init_src.js
@@ -80,13 +80,13 @@ if (master.Status === "dev") {
   //       maxAge: 1000 * 60 * 24 // 24 hours
   //     }
   //   }));
-  //   app.use(function(req, res, next) {
-  //     res.header('Access-Control-Allow-Credentials', true);
-  //     res.header('Access-Control-Allow-Origin', req.headers.origin);
-  //     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  //     res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-  //     next();
-  //   });
+  app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', "localhost:8080");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    next();
+  });
   app.use(session({
     secret: `${master.secret.sesson_secret}`,
     cookie: {
