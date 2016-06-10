f19c86ab5e66a114bd2b04a2c03956e7eb294a70
uriel <zhuoshengjiang@icloud.com>
2016-06-10 13:13:48 -0700

    aa

--- crawler_prod.sh
+++ crawler_prod.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-babel backend/crawler.js -o ../crawler/crawler.js 
-
+babel backend/crawler.js -o ../crawler/crawler.js
+cp backend/tools/hotelscombinedFileNameConverter.json ../crawler/tools/hotelscombinedFileNameConverter.json
